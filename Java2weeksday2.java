cc4e67481dafd6dce8d662a08c12d6ddc1c5ef65
Dao <vongocanhdao20082002@gmail.com>
2026-04-08 22:36:06 +0700

    fix code lan cuoi

--- Java2weeksday2.java
+++ Java2weeksday2.java
@@ -48,8 +48,8 @@ public class Java2weeksday2{
                 try {
                     System.out.print("Tuoi: ");
                     int ageInt = Integer.parseInt(sc.nextLine().trim());
-                    if( ageInt <= 0)
-                        throw new IllegalArgumentException("Tuoi phai lon hon 0!!!");
+                    if( ageInt <= 18)
+                        throw new IllegalArgumentException("Tuoi phai lon hon 18!!!");
                     else{
                         this.age = ageInt;
                         return;
@@ -61,7 +61,18 @@ public class Java2weeksday2{
                 }
             }
         }
-    }
+        @Override
+        public String toString(){
+            return String.format("Mssv: %s, Ho va ten: %s, Tuoi: %d.", this.mssv, this.name, this.age);
+        }
+        //Nhập thông tin sinh viên
+        public void inputStudent(Scanner sc){
+            System.out.println("-----Nhap thong tin sinh vien----");
+            setMssv(sc);
+            setName(sc);
+            setAge(sc);
+        }
+    }    
     //Check chỉ chữ - true
     public static boolean checkText(String text){
         try{
@@ -86,6 +97,8 @@ public class Java2weeksday2{
                 throw new IllegalArgumentException("Mssv phai bat dau bang SV...");
             else if(text.length() != 5)
                 throw new IllegalArgumentException("Mssv chi co 5 ky tu!!!");
+            else if(text.equals("SV000"))
+                throw new IllegalArgumentException("Mssv SV000 khong ton tai!!!");
             else{
                 char[] t = text.toCharArray(); 
                 for(int i=2; i < t.length; i++){
@@ -99,10 +112,40 @@ public class Java2weeksday2{
             return false;
         }
     }
+    //Tiếp tục dùng chức năng
+    public static boolean yesOrNo(Scanner sc){
+        while(true){
+            System.out.print("Ban co muon tiep tuc khong? (Y/N): ");
+            String yORn = sc.nextLine().toUpperCase().trim();
+            switch(yORn) {
+                case "Y" -> {return true;}
+                case "N" -> {return false;}
+                default -> System.err.println("Vui long nhap Y hoac N!!!");
+            }
+        }
+    }
+    //Main
     public static void main(String[] agrs){
         try(Scanner sc = new Scanner(System.in)){
             ArrayList<Student> studenArrayList = new ArrayList<>();
-            Student st = new Student();
+            while(true){ 
+                Student st = new Student();
+                st.inputStudent(sc);
+                studenArrayList.add(st);
+                System.out.println("Da luu thanh cong!!!");
+                if(!yesOrNo(sc))
+                    break;
+            }
+            //In thông tin sinh viên
+            int i = 1;
+            for(Student st : studenArrayList)
+            {
+                System.out.printf("----- Thong tin sinh vien %d----%n", i);
+                System.out.println(st.toString());
+                i++;
+            }
+            //hellooooo
+            System.err.println("Hello world!!!"); 
         }
         
     }
