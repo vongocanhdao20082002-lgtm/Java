09aa6b9d10a7b1316bac4cc78967d4226ae17126
Dao <vongocanhdao20082002@gmail.com>
2026-04-09 09:34:57 +0700

    update.3

--- Java2weeksday2.java
+++ Java2weeksday2.java
@@ -1,149 +1,143 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
+public class Java2weeksday2 {
 
-public class Java2weeksday2{
-    public static class Student{
-        //Thuộc tính
+    public static class Student {
         private String mssv;
         private String name;
         private int age;
         //Constructor
-        public Student(String mssv, String name, int age){
+        public Student(String mssv, String name, int age) {
+            setMssv(mssv);
+            setName(name);
+            setAge(age);
+        }
+        //Constructor mặc định
+        public Student() {}
+        //Getter
+        public String getMssv(){return mssv;}
+        public String getName(){return name;}
+        public int getAge(){return age;}
+        //Setter
+        public void setMssv(String mssv){
+            checkMssv(mssv);
             this.mssv = mssv;
+        }
+        public void setName(String name){
+            checkName(name);
             this.name = name;
+        }
+        public void setAge(int age){
+            checkAge(age);
             this.age = age;
         }
-        public Student(){}
-        //Getter
-        public String getMssv(){return this.mssv;}
-        public String getName(){return this.name;}
-        public int getAge(){return this.age;}
-        //Setter
-        public void setMssv(Scanner sc){
-            while(true){
-                System.out.print("Mssv: ");
-                String mssvStr = sc.nextLine().toUpperCase().trim();
-                //check mssv
-                if(checkMssv(mssvStr)){
-                    this.mssv = mssvStr;
-                    return;
+        @Override
+        public String toString() {
+            return String.format("Mssv: %s, Ho va ten: %s, Tuoi: %d.", mssv, name, age);
+        }
+        //check input
+        private void checkMssv(String text){
+            if (text.isEmpty())
+                throw new IllegalArgumentException("Khong duoc de trong!!!");
+            else if (!text.startsWith("SV"))
+                throw new IllegalArgumentException("Mssv phai bat dau bang SV...");
+            else if (text.length() != 5)
+                throw new IllegalArgumentException("Mssv chi co 5 ky tu!!!");
+            else if (text.equals("SV000"))
+                throw new IllegalArgumentException("Mssv SV000 khong ton tai!!!");
+            else{
+                for (int i = 2; i < text.length(); i++) {
+                if (!Character.isDigit(text.charAt(i)))
+                    throw new IllegalArgumentException("3 ky tu cuoi phai la so!!!");
                 }
             }
         }
-        public void setName(Scanner sc){
-            while(true){
-                System.out.print("Ho va ten: ");
-                String nameStr = sc.nextLine().trim();
-                //Có số thì bug
-                if(checkText(nameStr)==true){
-                    this.name = nameStr;
-                    return;
-                }
+        private void checkName(String text) {
+            if (text.isEmpty())
+                throw new IllegalArgumentException("Ten khong duoc de trong!!!");
+            for (char c : text.toCharArray()) {
+                if (Character.isDigit(c))
+                    throw new IllegalArgumentException("Ten khong co so!!!");
             }
         }
-        public void setAge(Scanner sc){
-            while(true){ 
+        private void checkAge(int age) {
+            if (age <= 18)
+                throw new IllegalArgumentException("Tuoi phai lon hon 18!!!");
+        }
+        //Input student
+        public void inputStudent(Scanner sc){
+            System.out.println("-----Nhap thong tin sinh vien----");
+            //Mssv
+            while(true){
+                try {
+                    System.out.print("Mssv: ");
+                    String mssvStr = sc.nextLine().toUpperCase().trim();
+                    setMssv(mssvStr);
+                    break;
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+            //Name
+            while(true){
+                try {
+                    System.out.print("Ho va ten: ");
+                    String nameStr = sc.nextLine().trim();
+                    setName(nameStr);
+                    break;
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+            // Age
+            while (true){
                 try {
                     System.out.print("Tuoi: ");
-                    int ageInt = Integer.parseInt(sc.nextLine().trim());
-                    if( ageInt <= 18)
-                        throw new IllegalArgumentException("Tuoi phai lon hon 18!!!");
-                    else{
-                        this.age = ageInt;
-                        return;
-                    }
-                } catch(NumberFormatException e) {
-                    System.err.println("Tuoi la so nguyen!!!");
-                }catch(IllegalArgumentException e) {
+                    String ageStr = sc.nextLine().trim();
+                    int age = Integer.parseInt(ageStr);
+                    setAge(age);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.err.println("Tuoi phai la so nguyen!!!");
+                } catch (Exception e) {
                     System.err.println(e.getMessage());
                 }
             }
         }
-        @Override
-        public String toString(){
-            return String.format("Mssv: %s, Ho va ten: %s, Tuoi: %d.", this.mssv, this.name, this.age);
-        }
-        //Nhập thông tin sinh viên
-        public void inputStudent(Scanner sc){
-            System.out.println("-----Nhap thong tin sinh vien----");
-            setMssv(sc);
-            setName(sc);
-            setAge(sc);
-        }
-    }    
-    //Check chỉ chữ - true
-    public static boolean checkText(String text){
-        try{
-            if(text.isEmpty())
-                throw new IllegalArgumentException("Ten khong duoc de trong!!!");
-            for(Character c : text.toCharArray())
-                if(Character.isDigit(c))
-                    throw new IllegalArgumentException("Ten khong co so!!!");
-            return true;
-            
-        }catch(IllegalArgumentException e){
-            System.err.println(e.getMessage());
-            return false;
-        }
     }
-    //Check format mssv SV001...
-    public static boolean checkMssv(String text){
-        try{
-            if(text.isEmpty())
-                throw new IllegalArgumentException("Khong duoc de trong!!!");
-            else if(!text.startsWith("SV"))
-                throw new IllegalArgumentException("Mssv phai bat dau bang SV...");
-            else if(text.length() != 5)
-                throw new IllegalArgumentException("Mssv chi co 5 ky tu!!!");
-            else if(text.equals("SV000"))
-                throw new IllegalArgumentException("Mssv SV000 khong ton tai!!!");
-            else{
-                char[] t = text.toCharArray(); 
-                for(int i=2; i < t.length; i++){
-                    if(!Character.isDigit(t[i]))
-                        throw new IllegalArgumentException("3 ky tu cuoi phai la so!!!");
-                }
-                return true;
-            }
-        }catch(IllegalArgumentException e){
-            System.err.println(e.getMessage());
-            return false;
-        }
-    }
-    //Tiếp tục dùng chức năng
+    //Hỏi tiếp tục hay dừng
     public static boolean yesOrNo(Scanner sc){
         while(true){
             System.out.print("Ban co muon tiep tuc khong? (Y/N): ");
             String yORn = sc.nextLine().toUpperCase().trim();
-            switch(yORn) {
-                case "Y" -> {return true;}
-                case "N" -> {return false;}
+            switch(yORn){
+                case "Y"-> {return true;}
+                case "N"-> {return false;}
                 default -> System.err.println("Vui long nhap Y hoac N!!!");
             }
         }
     }
     //Main
-    public static void main(String[] agrs){
+    public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
-            ArrayList<Student> studenArrayList = new ArrayList<>();
-            while(true){ 
+            ArrayList<Student> list = new ArrayList<>();
+            //Nhập thông tin sinh viên
+            while(true){
                 Student st = new Student();
                 st.inputStudent(sc);
-                studenArrayList.add(st);
+                list.add(st);
                 System.out.println("Da luu thanh cong!!!");
-                if(!yesOrNo(sc))
+                //Tiếp tục hay dừng
+                if (!yesOrNo(sc))
                     break;
             }
-            //In thông tin sinh viên
+            // Hiển thị thông tin sinh viên
             int i = 1;
-            for(Student st : studenArrayList)
-            {
-                System.out.printf("----- Thong tin sinh vien %d----%n", i);
-                System.out.println(st.toString());
-                i++;
+            for(Student st : list){
+                System.out.printf("----- Thong tin sinh vien %d ----%n", i++);
+                System.out.println(st);
             }
-        }//try-with-resources
+        }
     }
 }
